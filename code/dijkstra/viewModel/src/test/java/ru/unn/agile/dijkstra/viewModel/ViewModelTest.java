98bc54238463040b89818fd4a3e3f75adfb6a118
olgaIvanova <olka-lyapa@mail.ru>
2018-12-08 19:04:57 +0300

    refactoring

--- code/dijkstra/viewModel/src/test/java/ru/unn/agile/dijkstra/viewModel/ViewModelTest.java
+++ code/dijkstra/viewModel/src/test/java/ru/unn/agile/dijkstra/viewModel/ViewModelTest.java
@@ -1,6 +1,7 @@
 package ru.unn.agile.dijkstra.viewModel;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ru.unn.agile.dijkstra.model.Vertex;
@@ -10,8 +11,11 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ViewModelTest {
     private ViewModel viewModel;
@@ -30,7 +34,7 @@ public class ViewModelTest {
         this.viewModel = viewModel;
     }
 
-    public  String getResourceAsString(final String path) {
+    public String getResourceAsString(final String path) {
         try {
             URL url = getClass().getResource(path);
 
@@ -137,5 +141,79 @@ public class ViewModelTest {
         viewModel.calculate();
 
         assertEquals("Vertex id = 6 not contains in graph", viewModel.getStatus());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canGetExceptionForViewModelConstructorWithNullLogger() {
+
+        new ViewModel(null);
+    }
+
+    @Test
+    public void canGetLogIsEmptyAfterInitializations() {
+        List<String> log = viewModel.getLoggerList();
+
+        assertTrue(log.isEmpty());
+    }
+
+    @Test
+    public void canGetLogMessageOnSingleFieldChange() {
+        viewModel.matrixProperty().setValue(getResourceAsString("SimpleGraph.json"));
+
+        String message = viewModel.getLoggerList().get(0);
+        String expectedMessage = changedMessage();
+        Assert.assertTrue(message.contains(expectedMessage));
+    }
+
+    @Test
+    public void canGetLogContainsMultipleVectorChangedMessages() {
+        inputData();
+
+        List<String> log = viewModel.getLoggerList();
+        assertEquals(log.size(), 3);
+
+        String message = viewModel.getLoggerList().get(2);
+        String expectedMessage = changedMessage();
+        assertTrue(message.contains(expectedMessage));
+    }
+
+    @Test
+    public void canGetLogContainsCalculateMessage() {
+        inputData();
+
+        viewModel.calculate();
+
+        String message = viewModel.getLoggerList().get(viewModel.getLoggerList().size() - 1);
+        String expectedMessage = String.format(ViewModel.LogMessages.CALCULATED_DISTANCE,
+                viewModel.getMatrixProperty(),
+                viewModel.getStartVertexProperty(),
+                viewModel.getFinishVertexProperty());
+        assertTrue(message.contains(expectedMessage));
+    }
+
+    @Test
+    public void canGetLogPropertyIsEmptyAfterInit() {
+        assertEquals("", viewModel.getLog());
+        assertEquals("", viewModel.logProperty().get());
+    }
+
+    @Test
+    public void canGetViewModelWithoutLogger() {
+        ViewModel viewModel = new ViewModel();
+
+        assertNotNull(viewModel);
+    }
+
+    private String changedMessage() {
+        return String.format(ViewModel.LogMessages.VALUE_CHANGE,
+                viewModel.matrixProperty().get(),
+                viewModel.startVertexProperty().get(),
+                viewModel.finishVertexProperty().get());
+    }
+
+    private void inputData() {
+        viewModel.matrixProperty().setValue(getResourceAsString("SimpleGraph.json"));
+        viewModel.startVertexProperty().setValue("1");
+        viewModel.finishVertexProperty().setValue("2");
     }
 }
